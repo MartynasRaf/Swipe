3a318bf3911bc2a2e3450e6aaa13bf06e1137911
Martynas R <m.rafanavicius@gmail.com>
2021-10-06 23:25:06 +0300

    + Starter code

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import Deck from './src/Deck';
+
+const DATA = [
+	{
+		id: 1,
+		text: 'Card #1',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 2,
+		text: 'Card #2',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 3,
+		text: 'Card #3',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 4,
+		text: 'Card #4',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 5,
+		text: 'Card #5',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 6,
+		text: 'Card #6',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 7,
+		text: 'Card #7',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+	{
+		id: 8,
+		text: 'Card #8',
+		uri: 'https://picsum.photos/300/200?random=1.jpg',
+	},
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const renderCard = function (item) {
+		return (
+			<View
+				key={item.id}
+				style={{ margin: 10, padding: 5, borderWidth: 1 }}
+			>
+				<Image
+					style={{ height: 200, width: 300 }}
+					source={{ uri: item.uri }}
+				/>
+				<Text style={{ marginBottom: 10 }}>{item.text}</Text>
+				<Button
+					icon={{ name: 'code' }}
+					backgroundColor='#03A9F4'
+					title='View Now!'
+				/>
+			</View>
+		);
+	};
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Deck data={DATA} renderCard={renderCard} />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
---
+++ src/Deck.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Animated, PanResponder } from 'react-native';
+
+const Deck = ({ data, renderCard }) => {
+	const position = new Animated.ValueXY();
+
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderMove: (event, gesture) => {
+			position.setValue({ x: gesture.dx, y: gesture.dy });
+		},
+		onPanResponderRelease: () => {},
+	});
+
+	function renderCards() {
+		return data.map((item, index) => {
+			if (index === 0) {
+				return (
+					<Animated.View
+						style={position.getLayout()}
+						{...panResponder.panHandlers}
+					>
+						{renderCard(item)}
+					</Animated.View>
+				);
+			}
+
+			return renderCard(item);
+		});
+	}
+
+	return <View>{renderCards()}</View>;
+};
+
+export default Deck;
